aaea09913c571a700b452cd2d7e966fb78f7debe
Guoxu Wang <wgx.nuaa@gmail.com>
2013-04-30 10:38:38 -0400

    app controller

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -32,4 +32,40 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+	public $helpers = array('Html', 'Js', 'Session', 'Form');
+
+	var $components = array(
+		'Session',
+		'Auth' => array(
+			'loginRedirect' => array('controller' => 'dashboard', 'action' => 'index'),
+			'logoutRedirect' => array('controller' => 'courses', 'action' => 'index'),
+			'authError' => 'You must be logged in as a registered user to access that page.',
+			'authorize' => array('Controller'),
+		)
+	);
+		
+	public function isAuthorized() {
+		return true;
+	}
+	
+	public function beforeFilter() {
+		session_start();
+
+		$this->Auth->allow('');
+		$current_user = $this->Auth->user();
+		$this->set( 'logged_in', $this->Auth->loggedIn() );
+		$this->set( 'full_user_info', $this->Auth->user() );
+		$this->set( 'user_id', $this->Auth->user('id') );
+
+		if ($this->Auth->user('psuedonym') != '') {
+			$this->set( 'user_first_name', $this->Auth->user('psuedonym') );
+		} else {
+			$this->set( 'user_first_name', $this->Auth->user('email') );
+		}
+		
+	//	$grav_default = 'http://'.$_SERVER['HTTP_HOST'].'/img/oa_icon.png';
+	//	$grav_url = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $this->Auth->user('email') ) ) ) . "?d=" . urlencode( $grav_default ) . "&s=" . 50;
+	//	$grav_url="/user_files".$this->Auth->user('thumbnail');
+	//	$this->set( 'grav_img', $grav_url ); 
+	}
 }
