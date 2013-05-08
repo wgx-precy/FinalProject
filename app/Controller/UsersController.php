4eac0d8115ae46bbc92223d31501e0773d2da665
acex000 <hechuan2g@gmail.com>
2013-05-08 16:04:26 -0400

    refined notes display query

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -2,7 +2,7 @@
 class UsersController extends AppController {
 	public $name = 'Users';
 	public $component = array('RequestHandlerComponent');
-	public $uses = array('User','UserLocation','Comment');
+	public $uses = array('User','UserLocation','Comment','Note');
 
 
 	public function login() {
@@ -123,6 +123,46 @@ class UsersController extends AppController {
 		if($login != 'true'){
 			$this->redirect(array('controller'=>'Users','action'=>'login'));
 		}
+
+
+
+		$result = $this->Note->query("/*f-date,n-date*/
+select  distinct notes.nid, notes.uid, notes.time, notes.note, notes.like_value from users_filters, zips, filters_tags,notes,tags  
+where users_filters.uid = $id and  users_filters.district = zips.district and users_filters.fid = filters_tags.fid 
+and notes.nid = tags.nid and filters_tags.tag = tags.tag and users_filters.choice = 'date' 
+and month(users_filters.datestart)*100+day(users_filters.datestart) <= month(current_date())*100+day(current_date()) and month(users_filters.dateend)*100+day(users_filters.dateend) >= month(current_date())*100+day(current_date()) and notes.choice = 'date' 
+and month(notes.startdate)*100+day(notes.startdate) <= month(current_date())*100+day(current_date()) and month(notes.enddate)*100+day(notes.enddate) >= month(current_date())*100+day(current_date) and users_filters.state = 'lunch break' and zips.z_x1<=notes.nloc_x
+and zips.z_x2>=notes.nloc_x and zips.z_y2<=notes.nloc_y and zips.z_y1>=notes.nloc_y and hour(users_filters.timestart)*100+minute(users_filters.timestart) <= hour(current_time())*100+minute(users_filters.timestart) 
+and hour(users_filters.timeend)*100+minute(users_filters.timeend) >= hour(current_time())*100+minute(current_time()) and hour(notes.starttime)*100+minute(notes.starttime) <= hour(current_time())*100+minute(current_time()) and hour(notes.endtime)*100+minute(notes.endtime) >= hour(current_time())*100+minute(current_time()) and notes.ntype = 'public'
+union
+/*f-week, n-date*/
+select distinct notes.nid, notes.uid, notes.time, notes.note, notes.like_value from users_filters, zips, filters_tags,notes,tags  
+where users_filters.uid = $id and  users_filters.district = zips.district and users_filters.fid = filters_tags.fid 
+and notes.nid = tags.nid and filters_tags.tag = tags.tag and users_filters.choice = 'week' 
+and users_filters.week1 <= dayofweek(current_date()) and users_filters.week2 >= dayofweek(current_date()) and notes.choice = 'date' 
+and month(notes.startdate)*100+day(notes.startdate) <= month(current_date())*100+day(current_date()) and month(notes.enddate)*100+day(notes.enddate) >= month(current_date())*100+day(current_date) and users_filters.state = 'lunch break' and zips.z_x1<=notes.nloc_x 
+and zips.z_x2>=notes.nloc_x and zips.z_y2<=notes.nloc_y and zips.z_y1>=notes.nloc_y and hour(users_filters.timestart)*100+minute(users_filters.timestart) <= hour(current_time())*100+minute(users_filters.timestart) 
+and hour(users_filters.timeend)*100+minute(users_filters.timeend) >= hour(current_time())*100+minute(current_time()) and hour(notes.starttime)*100+minute(notes.starttime) <= hour(current_time())*100+minute(current_time()) and hour(notes.endtime)*100+minute(notes.endtime) >= hour(current_time())*100+minute(current_time()) and notes.ntype = 'public'
+union
+/*f-date, n-week*/
+select distinct notes.nid, notes.uid, notes.time, notes.note, notes.like_value from users_filters, zips, filters_tags,notes,tags  
+where users_filters.uid  = $id and  users_filters.district = zips.district and users_filters.fid = filters_tags.fid 
+and notes.nid = tags.nid and filters_tags.tag = tags.tag and users_filters.choice = 'date'
+and month(users_filters.datestart)*100+day(users_filters.datestart) <= month(current_date())*100+day(current_date()) and month(users_filters.dateend)*100+day(users_filters.dateend) >= month(current_date())*100+day(current_date()) and notes.choice = 'week' 
+and notes.week1 <= dayofweek(current_date()) and notes.week2 >= dayofweek(current_date()) and users_filters.state = 'lunch break' and zips.z_x1<=notes.nloc_x 
+and zips.z_x2>=notes.nloc_x and zips.z_y2<=notes.nloc_y and zips.z_y1>=notes.nloc_y and hour(users_filters.timestart)*100+minute(users_filters.timestart) <= hour(current_time())*100+minute(users_filters.timestart) 
+and hour(users_filters.timeend)*100+minute(users_filters.timeend) >= hour(current_time())*100+minute(current_time()) and hour(notes.starttime)*100+minute(notes.starttime) <= hour(current_time())*100+minute(current_time()) and hour(notes.endtime)*100+minute(notes.endtime) >= hour(current_time())*100+minute(current_time()) and notes.ntype = 'public'
+union
+/*f-week,n-week*/
+select distinct notes.nid, notes.uid, notes.time, notes.note, notes.like_value from users_filters, zips, filters_tags,notes,tags  
+where users_filters.uid  = $id and  users_filters.district = zips.district and users_filters.fid = filters_tags.fid 
+and notes.nid = tags.nid and filters_tags.tag = tags.tag and users_filters.choice = 'week' 
+and users_filters.week1 <= dayofweek(current_date()) and users_filters.week2 >= dayofweek(current_date()) and notes.choice = 'week' 
+and notes.week1 <= dayofweek(current_date()) and notes.week2 >= dayofweek(current_date()) and users_filters.state = 'lunch break' and zips.z_x1<=notes.nloc_x 
+and zips.z_x2>=notes.nloc_x and zips.z_y2<=notes.nloc_y and zips.z_y1>=notes.nloc_y and hour(users_filters.timestart)*100+minute(users_filters.timestart) <= hour(current_time())*100+minute(users_filters.timestart) 
+and hour(users_filters.timeend)*100+minute(users_filters.timeend) >= hour(current_time())*100+minute(current_time()) and hour(notes.starttime)*100+minute(notes.starttime) <= hour(current_time())*100+minute(current_time()) and hour(notes.endtime)*100+minute(notes.endtime) >= hour(current_time())*100+minute(current_time()) and notes.ntype = 'public'");
+		//echo date("Y/m/d");
+		print_r($result);
 	}
 	public function getlocation(){
 		$id = $this->Session->read('user.id');
